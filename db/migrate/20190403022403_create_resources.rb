d8d7621028f72fbf90593556cafbbe994962d778
Maddie Jones <maddyg91@gmail.com>
2019-04-02 20:39:30 -0600

    Adds test and migration for resources

---
+++ db/migrate/20190403022403_create_resources.rb
@@ -0,0 +1,11 @@
+class CreateResources < ActiveRecord::Migration[5.2]
+  def change
+    create_table :resources do |t|
+      t.string :name
+      t.string :category
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
