d596c837570d7010a4d4cb4bcfa50c70f4398ad1
Jimmy Smith <jaystansmith@gmail.com>
2019-04-02 22:09:07 -0600

    Fixes misspelling of va_locations address column name all tests passing

--- app/models/va_location.rb
+++ app/models/va_location.rb
@@ -1,2 +1,8 @@
 class VaLocation < ApplicationRecord
+  validates_presence_of :name,
+                        :phone,
+                        :address,
+                        :city,
+                        :state,
+                        :zip_code
 end
