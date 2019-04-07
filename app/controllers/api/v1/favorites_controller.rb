cfa4907582348e063e37118a0617787e660c26c6
Jimmy Smith <jaystansmith@gmail.com>
2019-04-07 10:38:44 -0600

    Added create functionality to favorites

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -7,9 +7,14 @@ class Api::V1::FavoritesController < ApplicationController
     render json: FavoriteSerializer.new(Favorite.find(params[:id]))
   end
 
+  def create
+    fav = Favorite.create(favorite_params)
+    render json: fav, status: 201
+  end
+
   def destroy
-    @fav = Favorite.find(params[:id])
-    @fav.destroy
+    fav = Favorite.find(params[:id])
+    fav.destroy
   end
 
   private
