c4759dea9f656b43d65467e728ed789c94263e7f
Jimmy Smith <jaystansmith@gmail.com>
2019-04-07 10:17:43 -0600

    Added delete functionality of favorites

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::FavoritesController < ApplicationController
   def show
     render json: FavoriteSerializer.new(Favorite.find(params[:id]))
   end
+
+  def destroy
+    @fav = Favorite.find(params[:id])
+    @fav.destroy
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:name, :category, :url, :phone)
+  end
 end
