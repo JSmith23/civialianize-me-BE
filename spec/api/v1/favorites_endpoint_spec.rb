a4bfe658147415822bfa30456e480ee7b1d09b98
Maddie Jones <maddyg91@gmail.com>
2019-04-10 13:05:58 -0600

    Adds logo and description to testing for favorites

--- spec/api/v1/favorites_endpoint_spec.rb
+++ spec/api/v1/favorites_endpoint_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Resources API' do
   it 'returns index of favorites' do
-    favorite_1= Favorite.create(name: 'my favorite_1', category: 'medical', url:'myfavorite_1.com', phone: "773-546-7920")
-    favorite_2= Favorite.create(name: 'my favorite_2', category: 'housing', url:'myfavorite_2.com', phone: "773-546-9875")
+    favorite_1= Favorite.create(name: 'my favorite_1', category: 'medical', url:'myfavorite_1.com', phone: "773-546-7920", logo:"image-2", description:"hello")
+    favorite_2= Favorite.create(name: 'my favorite_2', category: 'housing', url:'myfavorite_2.com', phone: "773-546-9875", logo:"image-1", description:"good resource")
 
 
     get "/api/v1/favorites"
@@ -15,15 +15,19 @@ describe 'Resources API' do
     expect(favorites_json["data"][0]["attributes"]["category"]).to eq("medical")
     expect(favorites_json["data"][0]["attributes"]["url"]).to eq("myfavorite_1.com")
     expect(favorites_json["data"][0]["attributes"]["phone"]).to eq("773-546-7920")
+    expect(favorites_json["data"][0]["attributes"]["logo"]).to eq("image-2")
+    expect(favorites_json["data"][0]["attributes"]["description"]).to eq("hello")
     expect(favorites_json["data"][1]["attributes"]["name"]).to eq("my favorite_2")
     expect(favorites_json["data"][1]["attributes"]["category"]).to eq("housing")
     expect(favorites_json["data"][1]["attributes"]["url"]).to eq("myfavorite_2.com")
     expect(favorites_json["data"][1]["attributes"]["phone"]).to eq("773-546-9875")
+    expect(favorites_json["data"][1]["attributes"]["logo"]).to eq("image-1")
+    expect(favorites_json["data"][1]["attributes"]["description"]).to eq("good resource")
   end
 
   it 'returns an individual favorite' do
 
-    favorite_1= create(:favorite, id: 1, name: "my favorite_1", category: "medical", url: "myfavorite_1.come", phone: "773-546-7920")
+    favorite_1= create(:favorite, id: 1, name: "my favorite_1", category: "medical", url: "myfavorite_1.come", phone: "773-546-7920", logo:"image-3", description: "medical resource")
     get "/api/v1/favorites/1"
 
     favorites_json = JSON.parse(response.body)
@@ -33,11 +37,13 @@ describe 'Resources API' do
     expect(favorites_json["data"]["attributes"]["category"]).to eq(favorite_1.category)
     expect(favorites_json["data"]["attributes"]["url"]).to eq(favorite_1.url)
     expect(favorites_json["data"]["attributes"]["phone"]).to eq(favorite_1.phone)
+    expect(favorites_json["data"]["attributes"]["logo"]).to eq(favorite_1.logo)
+    expect(favorites_json["data"]["attributes"]["description"]).to eq(favorite_1.description)
   end
 
   it 'can edit an individual favorite' do
 
-    favorite_1= create(:favorite, id: 1, name: "my favorite_1", category: "medical", url: "myfavorite_1.come", phone: "773-546-7920")
+    favorite_1= create(:favorite, id: 1, name: "my favorite_1", category: "medical", url: "myfavorite_1.come", phone: "773-546-7920", logo:"image-4", description:"hello")
 
     put "/api/v1/favorites/1", params: {name: "new_name", category: "housing", url: "www.google.com", phone: "773-226-5412"}
 
@@ -52,7 +58,7 @@ describe 'Resources API' do
 
   it 'can create an individual favorite' do
 
-    post "/api/v1/favorites", params: {name: "new_name", category: "housing", url: "www.google.com", phone: "773-226-5412"}
+    post "/api/v1/favorites", params: {name: "new_name", category: "housing", url: "www.google.com", phone: "773-226-5412", logo:"image-5", description:"housing resource"}
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
@@ -65,14 +71,11 @@ describe 'Resources API' do
 
   it 'can delete an individual favorite' do
 
-    favorite_1= create(:favorite, id: 1, name: "my favorite_1", category: "medical", url: "myfavorite_1.come", phone: "773-546-7920")
+    favorite_1= create(:favorite, id: 1, name: "my favorite_1", category: "medical", url: "myfavorite_1.come", phone: "773-546-7920", logo:"image-6", description:"no description")
 
     delete "/api/v1/favorites/1"
 
     expect(response).to be_successful
     expect(response.status).to eq(204)
   end
-
 end
-
-
